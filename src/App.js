4b25500c2dc0d53beb69bd95b0c1ffe046fdde3e
eerichmond02 <35665803+eerichmond02@users.noreply.github.com>
2018-02-02 12:14:43 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Users from './Users'
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Users</h1>
         </header>
+        <Users />
       </div>
     );
   }
--- src/Users.js
+++ src/Users.js
@@ -1,1 +1,27 @@
 // User component
+import React from 'react';
+
+const Users = (props) => {
+
+	return (
+			<div>
+				<table>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+						<th>Created At</th>
+						<th>Actions</th>
+					</tr>
+					<tr>
+						<td>Name</td>
+						<td>Email</td>
+						<td>Created At</td>
+						<td>Actions</td>
+					</tr>
+				</table>
+			</div>
+	);
+
+}
+
+export default Users;
